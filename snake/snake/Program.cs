12a848529d5ee28b549b3db0de4d232fc6f0b388
D4rkW4nderer <andron3443@gmail.com>
2018-10-25 20:58:13 +0300

    Add Class Figure and Vertical Line

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,6 +10,22 @@ namespace snake
     {
         static void Main(string[] args)
         {
+            Console.SetBufferSize(200, 80);
+
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
+
+            Point p = new Point(4, 5, '+');
+            p.Draw();
+
+            /*
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
@@ -18,6 +34,7 @@ namespace snake
 
             HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
             line.Drow();
+            */
         }
     }
 }
